99e79468c30c598c50b6e105c135cdd4b2061072
Sean Carstensen <seanlcarstensen@gmail.com>
2020-01-10 18:12:21 -0800

    implemented eval function. All step2 tests pass except for Test7 which should throw an error (unsure as to why at this point)

--- repl.py
+++ repl.py
@@ -1,17 +1,52 @@
 import reader
 import printer
 
+loop_env = {
+    "+": lambda x,y: x + y,
+    "-": lambda x,y: x - y,
+    "*": lambda x,y: x * y,
+    "/": lambda x,y: int(x / y)
+}
+
+def eval_ast(ast, env):
+#   import pdb; pdb.set_trace()
+   val = None
+   if isinstance(ast, list):
+       val = []
+       for ele in ast:
+           val.append(repl_eval(ele, env))
+   else:
+       sym = loop_env.get(ast)
+       if sym is not None:
+           val = sym
+       else:
+           val = ast
+
+   return val
+
 def repl_read(line):
     return reader.read_str(line)
 
-def repl_eval(expr):
-    return expr
+def repl_eval(ast, loop_env):
+#    import pdb; pdb.set_trace()
+    if isinstance(ast, list):
+      # do list stuff
+      if len(ast) == 0:
+          return ast
+      else:
+          ast = eval_ast(ast, loop_env)
+          if callable(ast[0]):
+              # TODO: make num args variable
+              ast = ast[0](ast[1], ast[2])
+    else:
+        ast = eval_ast(ast, loop_env)
+    return ast
 
 def repl_print(line):
     return printer.pr_str(line)
 
 def rep(line):
-    return repl_print(repl_eval(repl_read(line)))
+    return repl_print(repl_eval(repl_read(line), loop_env))
 
 def test_repl(line):
     return rep(line)
