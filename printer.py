6a8b62e2480a094c3dc2e2a2cc3a659c912797d3
Sean Carstensen <seanlcarstensen@gmail.com>
2020-01-25 13:39:52 -0800

    added print_readably param to pr_str function to print special chars as their escaped representations

--- printer.py
+++ printer.py
@@ -1,6 +1,8 @@
-def pr_str(mal):
+def pr_str(mal, print_readably=False):
     mal_str = ""
 
+    # if mal is a list, recursively unpack it into str representation
+    # if mal is not a list, return str representation
     if isinstance(mal, list):
         mal_str += "("
         for i, ele in enumerate(mal):
@@ -8,6 +10,23 @@ def pr_str(mal):
                 mal_str += " "
             mal_str += pr_str(ele)
         mal_str += ")"
-        return mal_str
+
     else:
-        return str(mal)
+        mal_str = str(mal)
+
+        if print_readably:
+            # look for special chars
+            for i, ch in enumerate(mal):
+
+                # rewrite special chars as escape sequences
+                if ch == "\"":
+                    mal_str = mal_str[:i] + "\\" + mal_str[i:]
+
+                if ch == "\n":
+                    mal_str = mal_str[:i] + "\\n" + mal_str[i+1:]
+
+                if ch == "\\":
+                    mal_str = mal_str[:i] + "\\" + mal_str[i:]
+
+
+    return mal_str
